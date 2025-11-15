ba1b00b266009a2d09c4061cce0b1c423c1f15f1
RG <144122470+rgngrc@users.noreply.github.com>
2025-11-15 22:37:47 +0800

    Add panels and buttons in Dashboard

--- FinalProject/DashboardScreen.Designer.cs
+++ FinalProject/DashboardScreen.Designer.cs
@@ -30,13 +30,24 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashboardScreen));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblNavbar = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblNavbar = new System.Windows.Forms.Label();
+            this.lblDashboard = new System.Windows.Forms.Label();
+            this.panelScannerStatus = new System.Windows.Forms.Panel();
+            this.panelScanToday = new System.Windows.Forms.Panel();
+            this.panelTotalScans = new System.Windows.Forms.Panel();
+            this.panelThisWeek = new System.Windows.Forms.Panel();
+            this.panelRecentScans = new System.Windows.Forms.Panel();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnStudentRecords = new System.Windows.Forms.Button();
+            this.btnScanHistory = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -56,69 +67,6 @@
             this.panel1.Size = new System.Drawing.Size(1542, 139);
             this.panel1.TabIndex = 0;
             // 
-            // lblNavbar
-            // 
-            this.lblNavbar.AutoSize = true;
-            this.lblNavbar.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNavbar.ForeColor = System.Drawing.Color.White;
-            this.lblNavbar.Location = new System.Drawing.Point(38, 47);
-            this.lblNavbar.Name = "lblNavbar";
-            this.lblNavbar.Size = new System.Drawing.Size(472, 41);
-            this.lblNavbar.TabIndex = 0;
-            this.lblNavbar.Text = " Student ID Scanning System";
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.White;
-            this.panel2.ForeColor = System.Drawing.Color.Transparent;
-            this.panel2.Location = new System.Drawing.Point(1054, 28);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(2, 83);
-            this.panel2.TabIndex = 1;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(724, 40);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(56, 59);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(793, 56);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(207, 32);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Roan University";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(1117, 57);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 28);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Admin";
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.White;
-            this.panel3.ForeColor = System.Drawing.Color.Transparent;
-            this.panel3.Location = new System.Drawing.Point(1263, 28);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(2, 83);
-            this.panel3.TabIndex = 2;
-            // 
             // btnLogout
             // 
             this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(50)))), ((int)(((byte)(80)))));
@@ -132,14 +80,193 @@
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = false;
             // 
-            // Dashboard
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.ForeColor = System.Drawing.Color.Transparent;
+            this.panel3.Location = new System.Drawing.Point(1263, 28);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(2, 83);
+            this.panel3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(1117, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Admin";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(793, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Roan University";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(724, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(56, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.ForeColor = System.Drawing.Color.Transparent;
+            this.panel2.Location = new System.Drawing.Point(1054, 28);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(2, 83);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblNavbar
+            // 
+            this.lblNavbar.AutoSize = true;
+            this.lblNavbar.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNavbar.ForeColor = System.Drawing.Color.White;
+            this.lblNavbar.Location = new System.Drawing.Point(38, 47);
+            this.lblNavbar.Name = "lblNavbar";
+            this.lblNavbar.Size = new System.Drawing.Size(472, 41);
+            this.lblNavbar.TabIndex = 0;
+            this.lblNavbar.Text = " Student ID Scanning System";
+            // 
+            // lblDashboard
+            // 
+            this.lblDashboard.AutoSize = true;
+            this.lblDashboard.Font = new System.Drawing.Font("Cambria", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDashboard.ForeColor = System.Drawing.Color.White;
+            this.lblDashboard.Location = new System.Drawing.Point(53, 177);
+            this.lblDashboard.Name = "lblDashboard";
+            this.lblDashboard.Size = new System.Drawing.Size(291, 34);
+            this.lblDashboard.TabIndex = 1;
+            this.lblDashboard.Text = "Dashboard Overview";
+            // 
+            // panelScannerStatus
+            // 
+            this.panelScannerStatus.BackColor = System.Drawing.Color.White;
+            this.panelScannerStatus.ForeColor = System.Drawing.Color.Transparent;
+            this.panelScannerStatus.Location = new System.Drawing.Point(59, 230);
+            this.panelScannerStatus.Name = "panelScannerStatus";
+            this.panelScannerStatus.Size = new System.Drawing.Size(319, 174);
+            this.panelScannerStatus.TabIndex = 2;
+            // 
+            // panelScanToday
+            // 
+            this.panelScanToday.BackColor = System.Drawing.Color.White;
+            this.panelScanToday.ForeColor = System.Drawing.Color.Transparent;
+            this.panelScanToday.Location = new System.Drawing.Point(431, 230);
+            this.panelScanToday.Name = "panelScanToday";
+            this.panelScanToday.Size = new System.Drawing.Size(319, 174);
+            this.panelScanToday.TabIndex = 3;
+            // 
+            // panelTotalScans
+            // 
+            this.panelTotalScans.BackColor = System.Drawing.Color.White;
+            this.panelTotalScans.ForeColor = System.Drawing.Color.Transparent;
+            this.panelTotalScans.Location = new System.Drawing.Point(1170, 230);
+            this.panelTotalScans.Name = "panelTotalScans";
+            this.panelTotalScans.Size = new System.Drawing.Size(319, 174);
+            this.panelTotalScans.TabIndex = 5;
+            // 
+            // panelThisWeek
+            // 
+            this.panelThisWeek.BackColor = System.Drawing.Color.White;
+            this.panelThisWeek.ForeColor = System.Drawing.Color.Transparent;
+            this.panelThisWeek.Location = new System.Drawing.Point(798, 230);
+            this.panelThisWeek.Name = "panelThisWeek";
+            this.panelThisWeek.Size = new System.Drawing.Size(319, 174);
+            this.panelThisWeek.TabIndex = 4;
+            // 
+            // panelRecentScans
+            // 
+            this.panelRecentScans.BackColor = System.Drawing.Color.White;
+            this.panelRecentScans.ForeColor = System.Drawing.Color.Transparent;
+            this.panelRecentScans.Location = new System.Drawing.Point(59, 489);
+            this.panelRecentScans.Name = "panelRecentScans";
+            this.panelRecentScans.Size = new System.Drawing.Size(1430, 192);
+            this.panelRecentScans.TabIndex = 3;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScan.Location = new System.Drawing.Point(109, 711);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(186, 55);
+            this.btnScan.TabIndex = 6;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            // 
+            // btnStudentRecords
+            // 
+            this.btnStudentRecords.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStudentRecords.Location = new System.Drawing.Point(498, 711);
+            this.btnStudentRecords.Name = "btnStudentRecords";
+            this.btnStudentRecords.Size = new System.Drawing.Size(186, 55);
+            this.btnStudentRecords.TabIndex = 7;
+            this.btnStudentRecords.Text = "Student Records";
+            this.btnStudentRecords.UseVisualStyleBackColor = true;
+            // 
+            // btnScanHistory
+            // 
+            this.btnScanHistory.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScanHistory.Location = new System.Drawing.Point(876, 711);
+            this.btnScanHistory.Name = "btnScanHistory";
+            this.btnScanHistory.Size = new System.Drawing.Size(186, 55);
+            this.btnScanHistory.TabIndex = 8;
+            this.btnScanHistory.Text = "Scan History";
+            this.btnScanHistory.UseVisualStyleBackColor = true;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.Location = new System.Drawing.Point(1255, 711);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(186, 55);
+            this.btnSettings.TabIndex = 9;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(50)))), ((int)(((byte)(80)))));
+            this.panel4.ForeColor = System.Drawing.Color.Transparent;
+            this.panel4.Location = new System.Drawing.Point(59, 430);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1430, 60);
+            this.panel4.TabIndex = 4;
+            // 
+            // DashboardScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(111)))), ((int)(((byte)(157)))));
             this.ClientSize = new System.Drawing.Size(1540, 781);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.btnScanHistory);
+            this.Controls.Add(this.btnStudentRecords);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.panelRecentScans);
+            this.Controls.Add(this.panelTotalScans);
+            this.Controls.Add(this.panelScanToday);
+            this.Controls.Add(this.panelThisWeek);
+            this.Controls.Add(this.panelScannerStatus);
+            this.Controls.Add(this.lblDashboard);
             this.Controls.Add(this.panel1);
-            this.Name = "Dashboard";
+            this.Name = "DashboardScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dashboard - Roan University";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -148,6 +275,7 @@
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -161,5 +289,16 @@
         private System.Windows.Forms.Button btnLogout;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDashboard;
+        private System.Windows.Forms.Panel panelScannerStatus;
+        private System.Windows.Forms.Panel panelScanToday;
+        private System.Windows.Forms.Panel panelTotalScans;
+        private System.Windows.Forms.Panel panelThisWeek;
+        private System.Windows.Forms.Panel panelRecentScans;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnStudentRecords;
+        private System.Windows.Forms.Button btnScanHistory;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Panel panel4;
     }
 }
